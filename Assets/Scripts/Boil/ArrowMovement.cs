c973663823f0547a582e12a763676e8136d589fb
whatPanache <pwcarney@bu.edu>
2017-07-10 21:45:31 -0400

    Boil first pass done

---
+++ Assets/Scripts/Boil/ArrowMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMovement : MonoBehaviour
+{
+    public float arrow_speed;
+
+    float click_cooldown = 0.2f;
+    float last_click = 0f;
+
+    int score = 0;
+
+	void Update ()
+    {
+        if (Input.GetMouseButton(0) && Time.timeSinceLevelLoad > last_click)
+        {
+            last_click = Time.timeSinceLevelLoad + click_cooldown;
+
+            GetComponent<Rigidbody2D>().AddForce(transform.up * arrow_speed);
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0f, Mathf.Clamp(GetComponent<Rigidbody2D>().velocity.y, -200f, 200f));
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        score++;
+    }
+}
---
+++ Assets/Scripts/Boil/ZoneMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneMovement : MonoBehaviour
+{
+    public float zone_speed;
+    float orig_zone_speed;
+    bool zoom_mode;
+
+    public float flip_cooldown = 2f;
+    float last_flip = 0f;
+
+    Vector3 dest;
+    Vector3 top_pos = new Vector3(-13f, 188.81f, 0f);
+    Vector3 bot_pos = new Vector3(-13f, -172.03f, 0f);
+    Vector3 last_pos;
+
+    void Start()
+    {
+        orig_zone_speed = zone_speed;
+
+        dest = bot_pos;
+        last_pos = GetComponent<RectTransform>().localPosition;
+    }
+
+    void Update()
+    {
+        GetComponent<RectTransform>().localPosition = Vector3.MoveTowards(GetComponent<RectTransform>().localPosition, dest, Time.deltaTime * zone_speed);
+
+        if (zoom_mode && GetComponent<RectTransform>().localPosition == dest)
+        {
+            zoom_mode = false;
+            zone_speed = orig_zone_speed;
+
+            dest = bot_pos;
+        }
+
+        if (GetComponent<RectTransform>().localPosition == last_pos)
+        {
+            zoom_mode = true;
+            zone_speed = zone_speed * 2;
+
+            dest = new Vector3(-13f, Random.Range(bot_pos.y, top_pos.y), 0f);
+        }
+
+		if (Time.timeSinceLevelLoad > last_flip)
+        {
+            last_flip = Time.timeSinceLevelLoad + flip_cooldown + Random.Range(-1f, 1f);
+
+            zone_speed = zone_speed * Random.Range(0.7f, 1.5f);
+
+            if (dest == top_pos)
+                dest = bot_pos;
+            else if (dest == bot_pos)
+                dest = top_pos;
+        }
+        last_pos = GetComponent<RectTransform>().localPosition;
+    }
+}
