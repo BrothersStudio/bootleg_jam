25d3d267a01987cd795ff7c5b29041d682853be8
whatPanache <pwcarney@bu.edu>
2017-07-29 21:08:54 -0400

    First pass at new town

--- Assets/Scripts/Town/PlayerTownController.cs
+++ Assets/Scripts/Town/PlayerTownController.cs
@@ -7,45 +7,21 @@ using UnityEngine.UI;
 public class PlayerTownController : MonoBehaviour
 {
     MainController main_controller;
-    int total_amount = 999;
-    public Text amount_box;
 
-    public float player_speed = 1f;
-    public LayerMask person_mask;
-    public LayerMask ground_mask;
+    Vector3 dest;
 
     AudioSource player_source;
-    public AudioClip[] footsteps_clips;
-    public AudioClip speaking_clip;
     public AudioClip coin_clip;
 
-    string slow_string;
-    public GameObject dialogue_box;
-    public GameObject choice_box;
-    public Camera town_cam;
+    public Button continue_button;
+    public Text gallons_text;
 
-    public bool fed;
-    bool fed_chance;
-    public GameObject results_background;
-
-    bool speaking;
-    bool choosing;
-    float next_talk;
-    float talk_cooldown = 0.5f;
-    Vector3 dest;
+    bool fed = false;
+    int food_cost = 20;
 
 	void Start ()
     {
-        fed = false;
-        fed_chance = false;
-
-        speaking = false;
-        choosing = false;
-        next_talk = 0f;
-
-        dest = transform.position;
-
-        player_source = GetComponent<AudioSource>();
+        dest = new Vector3(-1.74f, 0f, -7.32f);
 
         if (SceneManager.sceneCount > 1)
         {
@@ -55,7 +31,6 @@ public class PlayerTownController : MonoBehaviour
                 if (main_objects[i].name == "MainController")
                 {
                     main_controller = main_objects[i].GetComponent<MainController>();
-                    total_amount = main_controller.total_amount;
 
                     main_controller.camera_listener.enabled = false;
                     main_controller.event_system.SetActive(false);
@@ -64,190 +39,23 @@ public class PlayerTownController : MonoBehaviour
             }
         }
 
-        amount_box.text = "Gallons of Booze:\n" + total_amount.ToString();
+        continue_button.onClick.AddListener(() =>
+        {
+            main_controller.town_done = true;
+            main_controller.RunNext();
+        });
+
+        player_source = GetComponent<AudioSource>();
+
+        gallons_text.text = "Gallons of Booze:\n" + main_controller.total_amount;
     }
 
-    void FixedUpdate()
+    public void BuyFood()
     {
-        // Rotate to face destination
-        if (Vector3.Distance(dest, transform.position) > 1)
-        {
-            transform.rotation = Quaternion.LookRotation(dest - transform.position, new Vector3(0, 1, 0));
-            //transform.Rotate(0, -90, 0);
-        }
 
-        // Player dialogue
-        if (Input.GetMouseButton(0) && Time.timeSinceLevelLoad > next_talk)
-        {
-            Talk();
-        }
-
-        // Player travel
-        transform.position = Vector3.MoveTowards(transform.position, dest, player_speed * Time.deltaTime);
-
-        if (Input.GetMouseButton(1) && !speaking)
-        {
-            Ray ray = town_cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, person_mask))
-            {
-                return;
-            }
-
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground_mask))
-            {
-                Debug.DrawLine(ray.origin, hit.point);
-                dest = hit.point;
-                dest.y = 0;
-            }
-        }
-
-        // Footsteps
-        if (Vector3.Distance(transform.position, dest) > 0 && !player_source.isPlaying)
-        {
-            player_source.clip = footsteps_clips[Random.Range(0, footsteps_clips.Length)];
-            player_source.Play();
-        }
-    }
-
-    public void Talk()
-    {
-        PlayerDialogueController dialogue_controller = GetComponentInChildren<PlayerDialogueController>();
-        if (dialogue_controller.in_talk_range || dialogue_controller.in_exit_zone)
-        {
-            Dialogue next_dialogue = dialogue_controller.GetNextDialogue();
-            if (next_dialogue != null && !choosing)
-            {
-                dest = transform.position;
-                speaking = true;
-
-                if (next_dialogue.is_choice)
-                {
-                    choosing = true;
-
-                    AnimateText(choice_box, next_dialogue.line);
-                    choice_box.SetActive(true);
-                    next_talk = Time.timeSinceLevelLoad + talk_cooldown;
-
-                    Button[] buttons = choice_box.GetComponentsInChildren<Button>();
-
-                    buttons[0].onClick.RemoveAllListeners();
-                    buttons[0].onClick.AddListener(() =>
-                    {
-                        choosing = false;
-
-                        if (dialogue_controller.in_exit_zone)
-                        {
-                            if (!fed && !fed_chance)
-                            {
-                                fed_chance = true;
-                                dialogue_controller.dialogue_ind--;
-                                Talk();
-                            }
-                            else
-                            {
-                                CloseScene();
-                            }
-                        }
-                        else
-                        {
-                            int previous_amount = total_amount;
-                            total_amount -= next_dialogue.price;
-
-                            if (next_dialogue.is_food)
-                            {
-                                fed = true;
-                                fed_chance = true;
-                            }
-
-                            AnimateNumber(previous_amount, total_amount);
-                            choice_box.SetActive(false);
-                            dialogue_box.SetActive(true);
-                            next_talk = Time.timeSinceLevelLoad + talk_cooldown;
-
-                            if (next_dialogue.yes_line != "")
-                            {
-                                AnimateText(dialogue_box, next_dialogue.yes_line);
-                            }
-                            else
-                            {
-                                Talk();
-                            }
-                        }
-                    });
-
-                    buttons[1].onClick.RemoveAllListeners();
-                    buttons[1].onClick.AddListener(() =>
-                    {
-                        choosing = false;
-
-                        if (dialogue_controller.in_exit_zone)
-                        {
-                            EndDialogue();
-                        }
-                        else
-                        {
-                            choice_box.SetActive(false);
-                            dialogue_box.SetActive(true);
-                            next_talk = Time.timeSinceLevelLoad + talk_cooldown;
-
-                            if (next_dialogue.no_line != "")
-                            {
-                                AnimateText(dialogue_box, next_dialogue.no_line);
-                            }
-                            else
-                            {
-                                Talk();
-                            }
-                        }
-                    });
-                }
-                else
-                {
-                    next_talk = Time.timeSinceLevelLoad + talk_cooldown;
-
-                    AnimateText(dialogue_box, next_dialogue.line);
-                    dialogue_box.SetActive(true);
-                }
-            }
-            else if (!choosing)
-            {
-                EndDialogue();
-            }
-        }
-    }
-
-    void EndDialogue()
-    {
-        next_talk = Time.timeSinceLevelLoad + talk_cooldown;
-        GetComponentInChildren<PlayerDialogueController>().dialogue_ind = 0;
-        speaking = false;
-        choosing = false;
-
-        choice_box.SetActive(false);
-        dialogue_box.SetActive(false);
-    }
-
-    void AnimateText(GameObject box, string str)
-    {
-        StopAllCoroutines();
-        StartCoroutine(AnimateTextRoutine(box, str));
-    }
-
-    IEnumerator AnimateTextRoutine(GameObject box, string strComplete)
-    {
-        slow_string = "";
-        int i = 0;
-        while (i < strComplete.Length)
-        {
-            player_source.clip = speaking_clip;
-            player_source.Play();
-
-            slow_string += strComplete[i++];
-            box.GetComponentInChildren<Text>().text = slow_string;
-            yield return new WaitForSeconds(0.03f);
-        }
+        AnimateNumber(main_controller.total_amount, main_controller.total_amount - food_cost);
+        main_controller.total_amount -= food_cost;
+        fed = true;
     }
 
     void AnimateNumber(int previous_amount, int new_amount)
@@ -264,51 +72,20 @@ public class PlayerTownController : MonoBehaviour
             player_source.Play();
 
             i--;
-            amount_box.text = "Gallons of Booze:\n" + i.ToString();
+            gallons_text.text = "Gallons of Booze:\n" + i.ToString();
             yield return new WaitForSeconds(0.01f);
         }
     }
 
-    void CloseScene()
+    void Update()
     {
-        results_background.SetActive(true);
-
-        if (fed && ((10 - main_controller.current_difficulty) == 0))
+        // Rotate to face destination
+        if (Vector3.Distance(dest, transform.position) > 1)
         {
-            // Win state
-            results_background.transform.Find("Fed Text").gameObject.GetComponent<Text>().text = "You survived prohibition.\nYou are now a wealthy moonshine baron.";
-            results_background.transform.Find("Success/Remaining Text").gameObject.SetActive(false);
-
-            results_background.transform.Find("Success").gameObject.SetActive(false);
-            results_background.transform.Find("Failure").gameObject.GetComponentInChildren<Button>().onClick.AddListener(() =>
-            {
-                main_controller.QuitGame();
-            });
+            transform.rotation = Quaternion.LookRotation(dest - transform.position, new Vector3(0, 1, 0));
         }
-        if (fed)
-        {
-            // Next month state
-            results_background.transform.Find("Fed Text").gameObject.GetComponent<Text>().text = "You fed your family this month!";
-            results_background.transform.Find("Success/Remaining Text").gameObject.GetComponent<Text>().text = (10 - main_controller.current_difficulty).ToString() + " months to go";
 
-            results_background.transform.Find("Failure").gameObject.SetActive(false);
-            results_background.transform.Find("Success").gameObject.GetComponentInChildren<Button>().onClick.AddListener(() =>
-            {
-                main_controller.town_done = true;
-                main_controller.RunNext();
-            });
-        }
-        else
-        {
-            // Lose state
-            results_background.transform.Find("Fed Text").gameObject.GetComponent<Text>().text = "Game Over!\nYour family has starved and died.\nWhat was this even for...";
-            results_background.transform.Find("Success/Remaining Text").gameObject.SetActive(false);
-
-            results_background.transform.Find("Success").gameObject.SetActive(false);
-            results_background.transform.Find("Failure").gameObject.GetComponentInChildren<Button>().onClick.AddListener(() =>
-            {
-                main_controller.QuitGame();
-            });
-        }
+        // Player travel
+        transform.position = Vector3.MoveTowards(transform.position, dest, 1f * Time.deltaTime);
     }
 }
--- Assets/Scripts/Town/TownsPersonController.cs
+++ Assets/Scripts/Town/TownsPersonController.cs
@@ -2,19 +2,92 @@
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TownsPersonController : MonoBehaviour
 {
-    public int[] dialogue_id;
+    MainController main_controller;
 
+    public List<int> dialogue_id;
     [HideInInspector]
     public List<Dialogue> dialogue_lines;
+    int dialogue_ind = 0;
+
+    float next_talk = 0f;
+    float talk_cooldown = 0.5f;
+    string slow_string;
+
+    AudioSource source;
+    public AudioClip speaking_clip;
 
     void Start()
     {
-        for (int i = 0; i < dialogue_id.Length; i++)
+        if (SceneManager.sceneCount > 1)
+        {
+            GameObject[] main_objects = SceneManager.GetSceneByName("Main").GetRootGameObjects();
+            for (int i = 0; i < main_objects.Length; i++)
+            {
+                if (main_objects[i].name == "MainController")
+                {
+                    main_controller = main_objects[i].GetComponent<MainController>();
+                }
+            }
+        }
+
+        source = GetComponent<AudioSource>();
+
+        switch (main_controller.current_difficulty)
+        {
+            case 0:
+                dialogue_id.Add(10);
+                break;
+            case 1:
+                dialogue_id.Add(10);
+                break;
+            case 2:
+                dialogue_id.Add(10);
+                break;
+        }
+
+        for (int i = 0; i < dialogue_id.Count; i++)
         {
             dialogue_lines.Add(DialogueManager.current.GetDialogue(dialogue_id[i]));
         }
+
+        next_talk = Time.timeSinceLevelLoad + talk_cooldown;
+        AnimateText(dialogue_lines[dialogue_ind].line);
     }
+
+    void AnimateText(string str)
+    {
+        StopAllCoroutines();
+        StartCoroutine(AnimateTextRoutine(str));
+    }
+
+    IEnumerator AnimateTextRoutine(string strComplete)
+    {
+        slow_string = "";
+        int i = 0;
+        while (i < strComplete.Length)
+        {
+            source.clip = speaking_clip;
+            source.Play();
+
+            slow_string += strComplete[i++];
+            GetComponentInChildren<Text>().text = slow_string;
+            yield return new WaitForSeconds(0.03f);
+        }
+    }
+
+    public void NextText()
+    {
+        if (dialogue_ind < dialogue_lines.Count)
+        {
+            dialogue_ind++;
+            AnimateText(dialogue_lines[dialogue_ind].line);
+        }
+    }
+
+
 }
